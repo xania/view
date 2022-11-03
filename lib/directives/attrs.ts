6dc6d7de462abec49482b7e1f3aaf528fbc928da
Ibrahim ben Salah <Ibrahim.BenSalah@planonsoftware.com>
2022-11-03 10:09:44 +0100

    compile attrs into templateNode

--- lib/directives/attrs.ts
+++ lib/directives/attrs.ts
@@ -1,17 +1,19 @@
-import { RenderTarget } from 'lib/jsx';
+import { AttributeType, SetAttributeTemplate, TemplateType } from '../jsx';
 
 export interface AttrsProps {
   [name: string]: string;
 }
 export function Attrs(props: AttrsProps) {
-  return {
-    render(target: RenderTarget) {
-      if (target.setAttribute instanceof Function) {
-        for (let name in props) {
-          const value = props[name];
-          target.setAttribute(name, value);
-        }
-      }
-    },
-  };
+  const result: SetAttributeTemplate[] = [];
+  for (const k in props) {
+    result.push({
+      type: TemplateType.SetAttribute,
+      attr: {
+        name: k,
+        type: AttributeType.Attribute,
+        value: props[k],
+      },
+    });
+  }
+  return result;
 }
