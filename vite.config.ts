731346bdb17b5e3aae180c8a9daf86e49a270d40
Ibrahim ben Salah <Ibrahim.BenSalah@planonsoftware.com>
2022-12-08 23:04:37 +0100

    remove webpack

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+﻿// vite.config.ts
+import { defineConfig } from 'vite';
+
+import typescript from '@rollup/plugin-typescript';
+import path from 'path';
+import { typescriptPaths } from 'rollup-plugin-typescript-paths';
+
+export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: path.resolve(__dirname, './lib'),
+      },
+    ],
+  },
+  build: {
+    manifest: true,
+    minify: true,
+    reportCompressedSize: true,
+    lib: {
+      entry: path.resolve(__dirname, 'lib/index.ts'),
+      fileName: 'main',
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: [],
+      plugins: [
+        typescriptPaths({
+          preserveExtensions: true,
+        }),
+        typescript({
+          sourceMap: true,
+          declaration: true,
+          outDir: 'dist',
+        }),
+      ],
+    },
+  },
+});
