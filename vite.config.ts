f4860843303a6541cfbfc87b2afbe66813f304ed
Ibrahim ben Salah <Ibrahim.BenSalah@planonsoftware.com>
2022-12-29 20:25:24 +0100

    update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 ﻿// vite.config.ts
 import { defineConfig } from 'vite';
-
-import typescript from '@rollup/plugin-typescript';
 import path from 'path';
-import { typescriptPaths } from 'rollup-plugin-typescript-paths';
 
 export default defineConfig({
   resolve: {
@@ -15,26 +12,18 @@ export default defineConfig({
     ],
   },
   build: {
-    manifest: true,
     minify: true,
     reportCompressedSize: true,
     lib: {
       entry: path.resolve(__dirname, 'lib/index.ts'),
       fileName: 'main',
-      formats: ['es', 'cjs'],
+      formats: ['es'],
     },
     rollupOptions: {
       external: [],
-      plugins: [
-        typescriptPaths({
-          preserveExtensions: true,
-        }),
-        typescript({
-          sourceMap: true,
-          declaration: true,
-          outDir: 'dist',
-        }),
-      ],
+      output: {
+        sourcemap: true,
+      },
     },
   },
 });
