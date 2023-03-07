d39aebca8744f557e65e5e13365acd16d3401d9d
Ibrahim ben Salah <ibrahim.bensalah@xania.nl>
2023-03-07 09:46:11 +0100

    initial setup create-app

---
+++ packages/create-app/README.md
@@ -0,0 +1,15 @@
+# Create Xania
+
+Scaffolding Your First Xania Project
+
+## Usage
+
+```bash
+npx init @xania/app <name>
+```
+
+or
+
+```bash
+npx @xania/app <name>
+```
---
+++ packages/create-app/src/cli.ts
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+
+import { create } from "create-create-app";
+import { resolve } from "path";
+import degit from "degit";
+
+const emitter = degit("xania/view/templates", {
+  cache: false,
+  force: true,
+  verbose: true,
+});
+
+emitter.on("info", (info) => {
+  console.log(info.message);
+});
+
+const templateRoot = resolve(".xania");
+console.log(templateRoot);
+
+emitter.clone(templateRoot).then(() => {
+  create("create-xania", {
+    templateRoot,
+
+    promptForTemplate: true,
+    promptForAuthor: true,
+
+    promptForDescription: false,
+    promptForEmail: false,
+    promptForLicense: false,
+
+    // extra: {
+    //   template: {
+    //     type: "list",
+    //     choices: ["default", "submodule", "react+xania"],
+    //     describe: "select template",
+    //   },
+    // },
+
+    async after({ answers, run, template }) {
+      if (template === "gitmodule") {
+        await run("git submodule add https://github.com/xania/view.git xania");
+      }
+    },
+    caveat: `Welcome to xania App`,
+  });
+});
+
+// const git: SimpleGit = simpleGit();
+
+// git.listRemote(["--head"]).then((response) => {
+//   console.log(response);
+
+//   create("create-xania", {
+//     templateRoot,
+
+//     promptForTemplate: true,
+//     promptForAuthor: true,
+
+//     promptForDescription: false,
+//     promptForEmail: false,
+//     promptForLicense: false,
+
+//     extra: {
+//       template: {
+//         type: "list",
+//         choices: ["default", "submodule", "react+xania"],
+//         describe: "select template",
+//       },
+//     },
+
+//     async after({ answers, run, template }) {
+//       if (template === "gitmodule") {
+//         await run("git submodule add https://github.com/xania/view.git xania");
+//       }
+//     },
+//     caveat: `Welcome to xania App`,
+//   });
+// });
+
+// See https://github.com/uetchy/create-create-app/blob/master/README.md for other options.
