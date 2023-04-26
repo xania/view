2fcee80f2a22f118bbe88656e1b35d4f96135f68
Ibrahim ben Salah <ibrahim.bensalah@xania.nl>
2023-04-26 08:15:01 +0200

    reverst dist

--- packages/kitchen-sink/dist/assets/index-13d1bfd1.js
+++
@@ -1,1 +0,0 @@
-import{j as e,P as l,T as a,F as c,S as o}from"./index-61d66076.js";function s(){const n=new o(0);return e(c,{children:e(l,{class:"flex-auto",children:[e(a,{children:"Counter"}),e("div",{class:"align-middle flex justify-center p-4",children:e("button",{class:"bg-yellow-500 font-bold py-2 px-4 rounded",click:n.update(t=>t+1),children:["Count: ",n]})})]})})}export{s as App};
--- packages/kitchen-sink/dist/assets/utils-ff539ce6.js
+++
@@ -1,1 +0,0 @@
-function o(e,t=400){return new Promise(n=>{setTimeout(()=>n(e),t)})}export{o as d};
