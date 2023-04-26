6f2315db3146b921e1714a104081bfc41df2b02a
Ibrahim ben Salah <ibrahim.bensalah@xania.nl>
2023-04-26 08:14:14 +0200

    [kitchen-sink] vercel build script

---
+++ packages/kitchen-sink/dist/assets/utils-ff539ce6.js
@@ -0,0 +1,1 @@
+function o(e,t=400){return new Promise(n=>{setTimeout(()=>n(e),t)})}export{o as d};
---
+++ packages/kitchen-sink/dist/pages/hello-world.tsx
@@ -0,0 +1,28 @@
+﻿class State {
+  private observers: any[] = [];
+  constructor(public value: number) {}
+
+  set(fn: Function) {
+    this.value = fn(this.value);
+    for (const obs of this.observers) {
+      obs.next(this.value);
+    }
+  }
+
+  subscribe(obs) {
+    this.observers.push(obs);
+    return function () {};
+  }
+}
+
+export function view() {
+  const counter = new State(1);
+  counter.subscribe({
+    next(c) {
+      console.log("counter", c);
+    },
+  });
+  return function client() {
+    setInterval(() => counter.set((x) => x + 1), 1000);
+  };
+}
