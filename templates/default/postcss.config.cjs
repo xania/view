d4fbf897c994e682daea35e01270b48e02f53913
Ibrahim ben Salah <ibrahim.bensalah@xania.nl>
2023-05-02 16:57:44 +0200

    [templates] fix tailwind config

---
+++ templates/default/postcss.config.cjs
@@ -0,0 +1,8 @@
+﻿const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+module.exports = config;
---
+++ templates/default/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+const pageDuration = '.3s';
+
+export default {
+  content: ["../**/*.html", "../**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "class",
+
+};
